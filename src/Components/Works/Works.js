2ee2f9266c78ddf8a82c87241adf2e7084d9cbe0
Gaurang gupta <g12172001@gmail.com>
2024-10-26 13:01:52 +0530

    Update Works.js
    Optional chaining

--- src/Components/Works/Works.js
+++ src/Components/Works/Works.js
@@ -8,7 +8,7 @@ function Works() {
             Portfolio <div className='w-[50%] h-[1px] ml-8 styleHeading'></div>
         </h1>
         <div className='grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 pt-4 gap-3'>
-            {categoriesObject.categories.map(cat => (
+            {categoriesObject?.categories?.map(cat => (
                 <Link to={`/works/${cat.name}`} className="rounded-3xl p-px bg-gradient-to-b from-[#85FFBD] to-[#FFFB7D] cursor-pointer">
                     <div className="bg-gray-50 p-10 rounded-[calc(1.5rem-1px)] flex flex-col items-center">
                         <img className="h-12 w-12 object-contain block" src={cat.image} alt={cat.alt} />
