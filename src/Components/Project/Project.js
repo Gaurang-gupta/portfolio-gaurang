2c3cf870d4616e0975d56c791f2a5cf3b6e5eb95
Gaurang-gupta <g12172001@gmail.com>
2024-10-26 16:59:46 +0530

    Changed styling and error correction

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -8,14 +8,18 @@ function Project() {
   const path = window.location.pathname.split("/")
   const projectsData = projects[path[2]]
   return (
-    <main className='xl:px-12 xl:py-10 xs:px-8 xs:py-6 text-justify w-full'>
-      <Link to={"/works"} className='flex items-center text-2xl pb-2'>
-        <IoMdArrowBack/>
-      </Link>
-      <h1 className='sm:text-4xl font-bold pb-4 flex items-center xs:text-xl'>
-        {path[2]} Projects <div className='xs:hidden sm:block w-[50%] h-[1px] ml-8 styleHeading'></div>
-      </h1>
-
+    <main className='xl:px-12 xl:pb-10 xs:px-8 xs:pb-6 text-justify w-full'>
+      <div className='sticky top-0 xl:pt-10 xs:pt-6 text-justify w-full bg-white'>
+        <Link to={"/works"} className='flex items-center text-2xl pb-2'>
+          <IoMdArrowBack/>
+        </Link>
+        
+        <h1 className='sm:text-4xl font-bold pb-4 flex items-center xs:text-xl'>
+          {path[2]} Projects <div className='xs:hidden sm:block w-[50%] h-[1px] ml-8 styleHeading'></div>
+        </h1>
+      </div>
+      
+      <div>
       {projectsData?.map(project => (
         <ProjectCard 
           key={project.id}
@@ -25,6 +29,8 @@ function Project() {
           deployment={project.deployment}
         />
       ))}
+      </div>
+      
     </main>
   )
 }
